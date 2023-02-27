147a934c99209b51654305d213ea529de4663552
dylankhiew <dylankhiew@gmail.com>
2023-02-27 22:21:41 +0800

    chore: add typings for risk graph

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,4 +1,5 @@
 export namespace app {
+  //#region LOGIN SCREEN
   interface PasswordStrength {
     qualifiedEntries: number;
     isPasswordStrong: boolean;
@@ -13,4 +14,22 @@ export namespace app {
     width: string;
     backgroundColor: string;
   }
+
+  interface RiskGraphLabel {
+    title: string;
+    color: string;
+  }
+  //#endregion LOGIN SCREEN
+
+  //#region RISK GRAPH SCREEN
+  interface LastPlacementCoordinates {
+    x: number;
+    y: number;
+  }
+
+  type RiskGraphContextType = {
+    lastPlacementCoordinates: LastPlacementCoordinates;
+    setLastPlacementCoordinates: (value: app.LastPlacementCoordinates) => void;
+  };
+  //#endregion RISK GRAPH SCREEN
 }
