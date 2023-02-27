8c7a501df7d41b6192e4f40514a81c9dfb179739
dylankhiew <dylankhiew@gmail.com>
2023-02-27 22:22:01 +0800

    chore: add constants for risk graph

---
+++ src/pages/risk-graph/constants/riskGraphConstants.ts
@@ -0,0 +1,16 @@
+import { app } from "@/typings";
+
+export const RISK_GRAPH_LABELS: app.RiskGraphLabel[] = [
+  {
+    title: 'RISK',
+    color: 'tomato',
+  },
+  {
+    title: 'REWARD',
+    color: 'orangered',
+  },
+  {
+    title: 'MONEY',
+    color: 'darkgreen',
+  },
+];
